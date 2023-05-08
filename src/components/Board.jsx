77c75e0482f66878f0181b60bc4868e1156fea01
Johnnie <shadyman32@gmail.com>
2023-05-08 16:33:53 +0500

    added openField function

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,44 @@
 import { Square } from "./Square";
 
 export function Board({ squares, setSquares }) {
-    function handleClick(e, rowId, columnId) {
+    function handleClick(e, y, x) {
         const updateSquares = squares.slice();
         if (e.type === 'click') {
-            updateSquares[rowId][columnId].open = true;
+            updateSquares[y][x].open = true;
+            openField(updateSquares, y, x)
         } else if (e.type === 'contextmenu') {
             e.preventDefault()
-            updateSquares[rowId][columnId].flagged = true;
+            updateSquares[y][x].flagged = true;
         }
+        setSquares(updateSquares);
+    }
+
+    function openField(updateSquares, y, x) {
+        const currentRow = updateSquares[y];
+        if (!currentRow[x].open) {
+            currentRow[x].open = true;
+        }
+
+        if (currentRow[x].minesNearby > 0 || currentRow[x].mine) return;
+
+        if (x < currentRow.length - 1 && !currentRow[x + 1].open) {
+            openField(updateSquares, y, x + 1);
+        }
+
+        if (x > 0 && !currentRow[x - 1].open) {
+            openField(updateSquares, y, x - 1);
+        }
+
+        const topRow = updateSquares[y - 1];
+        if (topRow && !topRow[x].open) {
+            openField(updateSquares, y - 1, x);
+        }
+
+        const bottomRow = updateSquares[y + 1];
+        if (bottomRow) {
+            openField(updateSquares, y + 1, x);
+        }
+
         setSquares(updateSquares);
     }
 
