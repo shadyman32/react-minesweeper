af89465257c0d7259cb37f75fa1114e726f72635
Johnnie <shadyman32@gmail.com>
2023-06-08 22:49:55 +0500

    create root route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './styles.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App >,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <RouterProvider router={router}></RouterProvider>
 );
